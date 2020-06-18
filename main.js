a0d0a266b1bdc73221e87975ea7df6881441a673
huimarco <65921888+huimarco@users.noreply.github.com>
2020-06-18 16:28:28 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,24 @@
+function openNav() {
+    document.getElementById('mySideBar').style.width = '250px';
+}
+
+function closeNav() {
+    document.getElementById('mySideBar').style.width = '0';
+}
+
+function myFunction() {
+    document.body.classList.toggle('light-mode');
+ }
+
+ var x = 0;
+
+ function reveal() {
+     if (x==0) {
+        document.getElementById('demo').classList.remove('hidden');
+        x = 1;
+     }
+     else {
+        document.getElementById('demo').classList.add('hidden');
+        x = 0;
+     }
+ }
